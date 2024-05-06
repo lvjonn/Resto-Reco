e4af4a092b835ea293c41eecd5756bc044e1852e
aljonn <santosaljonn13@gmail.com>
2024-05-06 18:41:45 +1000

    restructured menu view

--- RestoReco/RestoReco/View/MenuView.swift
+++ RestoReco/RestoReco/View/MenuView.swift
@@ -4,47 +4,63 @@ struct MenuView: View {
     @EnvironmentObject var restoViewModel: RestoViewModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "fork.knife")
-                .foregroundColor(.black)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("RestoReco")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-                .padding(.bottom, 20)
-            
-            List(restoViewModel.restaurants, id: \.id) { restaurant in
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(restaurant.name)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.black)
-                    Text("Rating: \(restaurant.rating)")
-                        .foregroundColor(.gray)
-                    Text("Reviews: \(restaurant.reviewCount)")
-                        .foregroundColor(.gray)
-                    Text("Price: \(restaurant.price ?? "N/A")")
-                        .foregroundColor(.gray)
-                    
-                    AsyncImage(url: URL(string: restaurant.imageUrl ?? "")) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    } placeholder: {
-                        ProgressView()
+        NavigationStack{
+            VStack {
+                Image(systemName: "fork.knife")
+                    .foregroundColor(.black)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Text("RestoReco")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .padding(.bottom, 20)
+                
+                List(restoViewModel.restaurants, id: \.id) { restaurant in
+                    NavigationLink(destination: RestaurantDetailsView(/*pass in restaurant*/)){
+                        VStack(alignment: .leading, spacing: 8) {
+                            HStack {
+                                AsyncImage(url: URL(string: restaurant.imageUrl ?? "")) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(8)
+                                
+                                Spacer()
+                                
+                                VStack{
+                                    Text(restaurant.name)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.black)
+                                    
+                                    Text("Rating: \(restaurant.rating)")
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.leading)
+                                    Text("Reviews: \(restaurant.reviewCount)")
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.leading)
+                                    Text("Price: \(restaurant.price ?? "N/A")")
+                                        .foregroundColor(.gray)
+                                        .multilineTextAlignment(.leading)
+                                }
+                                Spacer()
+                                
+                            }
+                        }
+                        .padding(.vertical, 8)
                     }
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(8)
                 }
-                .padding(.vertical, 8)
+                .listStyle(PlainListStyle())
+                .padding(.horizontal)
             }
-            .listStyle(PlainListStyle())
-            .padding(.horizontal)
+            .padding()
         }
-        .padding()
     }
 }
 
