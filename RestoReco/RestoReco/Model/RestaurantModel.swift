22df5319983b6df2830f77d4801e5638190e5994
aljonn <santosaljonn13@gmail.com>
2024-05-09 22:13:06 +1000

    Updated details of RestaurantModel

--- RestoReco/RestoReco/Model/RestaurantModel.swift
+++ RestoReco/RestoReco/Model/RestaurantModel.swift
@@ -7,14 +7,50 @@
 
 import Foundation
 
-struct RestaurantModel: Codable{
-    let id: String
-    let name: String
-    let isClosed: Bool
-    let reviewCount: Int
-    let rating: Double
-    let price: String?
-    let phone: String
-    let imageUrl: String?
+struct RestaurantModel: Identifiable, Codable {
+    var id: String
+    var alias: String
+    var name: String
+    var imageUrl: String?
+    var isClosed: Bool
+    var url: String
+    var reviewCount: Int
+    var categories: [Category]
+    var rating: Double
+    var coordinates: Coordinates
+    var transactions: [String]
+    var price: String?
+    var location: Location
+    var phone: String
+    var displayPhone: String
+    var distance: Double
+    var attributes: Attributes
     
+    struct Category: Codable {
+        var alias: String
+        var title: String
+    }
+    
+    struct Coordinates: Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+    struct Location: Codable {
+        var address1: String
+        var address2: String?
+        var address3: String?
+        var city: String
+        var zipCode: String
+        var country: String
+        var state: String
+        var displayAddress: [String]
+    }
+    
+    struct Attributes: Codable {
+        var businessTempClosed: Bool?
+        var menuUrl: String?
+        var open24Hours: Bool?
+        var waitlistReservation: Bool?
+    }
 }
