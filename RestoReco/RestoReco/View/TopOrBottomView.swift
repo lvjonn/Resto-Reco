ff40fbcd162c5c3a4ab07e41e4e3440430a6e254
aljonn <santosaljonn13@gmail.com>
2024-05-12 02:47:09 +1000

    updated topOrbottom

--- RestoReco/RestoReco/View/TopOrBottomView.swift
+++ RestoReco/RestoReco/View/TopOrBottomView.swift
@@ -13,94 +13,86 @@ struct TopOrBottomView: View {
     @State private var topOrBottom = ""
     
     var body: some View {
-        VStack{
-            if choiceCount < 5{
-                VStack(alignment: .leading, spacing: 8) {
-                    Button(action: {
-                        choiceCount += 1
-                        topOrBottom = "top"
-                        restoViewModel.changeOptionTwo()
-                    }){
-                        if let unwrapped = restoViewModel.optionOne{
-                            RestaurantOverview(restaurant: unwrapped)
+        NavigationStack{
+            VStack{
+                if choiceCount < 5{
+                    VStack(alignment: .leading, spacing: 8) {
+                        Button(action: {
+                            choiceCount += 1
+                            topOrBottom = "top"
+                            restoViewModel.changeOptionTwo()
+                        }){
+                            if let unwrapped = restoViewModel.optionOne{
+                                RestaurantOverview(restaurant: unwrapped)
+                            }
+                            
+                        }
+                        .padding()
+                        
+                        HStack{
+                            Spacer()
+                            Image(systemName: "arrow.up.arrow.down.circle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 50, maxHeight: 50)
+                                .foregroundColor(.red)
+                                .foregroundStyle(.tint)
+                                .padding(.bottom)
+                            Spacer()
+                        }
+                        .padding(20)
+                        
+                        Button(action: {
+                            choiceCount += 1
+                            topOrBottom = "bottom"
+                            restoViewModel.changeOptionOne()
+                        }){
+                            if let unwrapped = restoViewModel.optionTwo{
+                                RestaurantOverview(restaurant: unwrapped)
+                            }
+                        }
+                        .padding()
+                    }
+                    .padding(.vertical, 8)
+                }else{
+                    //choice view
+                    VStack{
+                        if topOrBottom == "top"{
+                            if let unwrapped = restoViewModel.optionOne{
+                                NavigationLink(destination: RestaurantDetailsView(restaurant: unwrapped)){
+                                    RestaurantOverview(restaurant: unwrapped)
+                                        .padding()
+                                }
+                            }
+                        }else{
+                            if let unwrapped = restoViewModel.optionTwo{
+                                NavigationLink(destination: RestaurantDetailsView(restaurant: unwrapped)){
+                                    RestaurantOverview(restaurant: unwrapped)
+                                        .padding()
+                                }
+                            }
+                        }
+                        Button(action:{
+                            choiceCount = 0
+                        }){
+                            VStack{
+                                Image(systemName: "restart.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: 50, maxHeight: 50)
+                                    .foregroundColor(.red)
+                                    .foregroundStyle(.tint)
+                                Text("Restart")
+                                    .foregroundStyle(Color(.red))
+                            }
                         }
                         
                     }
-                    .padding()
                     
-                    HStack{
-                        Spacer()
-                        Image(systemName: "arrow.up.arrow.down.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxWidth: 50, maxHeight: 50)
-                            .foregroundColor(.red)
-                            .foregroundStyle(.tint)
-                            .padding(.bottom)
-                        Spacer()
-                    }
-                    .padding(20)
-                    
-                    Button(action: {
-                        choiceCount += 1
-                        topOrBottom = "bottom"
-                        restoViewModel.changeOptionOne()
-                    }){
-                        if let unwrapped = restoViewModel.optionTwo{
-                            RestaurantOverview(restaurant: unwrapped)
-                        }
-                    }
-                    .padding()
                 }
-                .padding(.vertical, 8)
-            }else{
-                //choice view
-                VStack{
-                    if topOrBottom == "top"{
-                        if let unwrapped = restoViewModel.optionOne{
-                            RestaurantOverview(restaurant: unwrapped)
-                                .padding()
-                            Button(action:{
-                                choiceCount = 0
-                            }){
-                                VStack{
-                                    Image(systemName: "restart.circle.fill")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(maxWidth: 50, maxHeight: 50)
-                                        .foregroundColor(.red)
-                                        .foregroundStyle(.tint)
-                                    Text("Restart")
-                                        .foregroundStyle(Color(.red))
-                                }
-                            }
-                        }
-                    }else{
-                        if let unwrapped = restoViewModel.optionTwo{
-                            RestaurantOverview(restaurant: unwrapped)
-                                .padding()
-                            Button(action:{
-                                choiceCount = 0
-                            }){
-                                VStack{
-                                    Image(systemName: "restart.circle.fill")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(maxWidth: 50, maxHeight: 50)
-                                        .foregroundColor(.red)
-                                        .foregroundStyle(.tint)
-                                    Text("Restart")
-                                        .foregroundStyle(Color(.red))
-                                }
-                            }
-                            .padding()
-                        }
-                    }
-                }
-                
             }
         }
-
+        
     }
 }
 
